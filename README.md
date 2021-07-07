8dc4d2b18f2643020b80761b1e30c34ef2cfffca
uncleallan <allan.rosen@btinternet.com>
2021-07-07 14:13:52 +0100

    Added some file changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pythonn
 Python files for updating and sharing
+
+This is an added line by us.
---
+++ try_factory.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+m=np.array([[5,  12,  6],[-3, 0, 14]])
+
+print(type(m))
+
+
+# In[4]:
+
+
+print(m.shape)
+
+
+# In[5]:
+
+
+print(55)
+
